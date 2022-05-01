f7641d0bdd16213bfff43e52010c5f6a9bc636b7
thelowlyClutch <armandhrubo@gmail.com>
2022-05-01 17:34:24 +0300

    -commit

---
+++ android/rider/app/src/main/java/com/rickshawpuller/r/riderapp/common/ConfigApp.java
@@ -0,0 +1,10 @@
+package com.rickshawpuller.r.riderapp.common;
+
+public class ConfigApp {
+    public static final double baseFare = 2.55;
+    public static final double timeRate = 0.35;
+    public static final double distanceRate = 1.75;
+    public static final String CLOUD_MESSAGING_SERVER_KEY = "AAAAG9tv7Jc:APA91bE6M7Gd59GF-IJ6Oqxov7syXhxVOcu3ov5z7kKiDn6ZigfO0SKK_lxdjN-gt4gLlr-QtaiLiSdOw1H_Dj7yhdgx2IpV3n7yR1LjtjtrN9VRk26N9TMz1xQDzqUbdo8tMf2Xalhk";
+    public static final String GOOGLE_API_KEY = "AIzaSyARiMep7MWawDd7wyJjddOzvWNpNcpurCY";
+
+}
